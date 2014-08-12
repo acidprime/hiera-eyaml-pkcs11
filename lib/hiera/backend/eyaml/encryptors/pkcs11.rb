b832c839d7dcdb146f51376a7a82d4178e8ea793
Zack Smith <zack.smith@puppetlabs.com>
2014-08-12 10:11:10 +1000

    Add slot id + lint
    Prior to this commit we hardcoded the slot id for the hsm. This commit updates
    the value to be defineable

--- lib/hiera/backend/eyaml/encryptors/pkcs11.rb
+++ lib/hiera/backend/eyaml/encryptors/pkcs11.rb
@@ -12,16 +12,17 @@ class Hiera
           include PKCS11
           self.options = {
 
+            :slot_id => { :desc    => "The slot to use for the session",
+                            :type    => :integer,
+                            :default => 4 },
 
             :key_label => { :desc    => "The label of the public/private key to use",
                             :type    => :string,
                             :default => "badkeyname" },
 
-
             :offline => { :desc    => "Work in offline mode using offline publickey",
                           :type    => :boolean,
                           :default => false },
-
 
             :offline_publickey => { :desc    => "Local path to the Public key used in offline mode",
                                     :type    => :string,
@@ -57,7 +58,8 @@ class Hiera
             hsm_usertype  = self.option :hsm_usertype
             hsm_password  = self.option :hsm_password
             hsm_library   = self.option :hsm_library
-            key_label     = self.option :key_label
+	    key_label     = self.option :key_label
+            slot_id       = self.option :slot_id
             raise StandardError, "hsm_usertype is not defined"  unless hsm_usertype
             raise StandardError, "hsm_password is not defined"  unless hsm_password
             raise StandardError, "hsm_library is not defined"   unless hsm_library
@@ -66,8 +68,9 @@ class Hiera
 
             puts pkcs11.info  # => #<PKCS11::CK_INFO cryptokiVersion=...>
             puts "SLOTS: #{pkcs11.active_slots}"
-
-            pkcs11.active_slots[3].open do |session|
+            
+            # Convert slotid from Human to array value
+            pkcs11.active_slots[(slot_id - 1 )].open do |session|
               session.login(hsm_usertype,hsm_password)
               
               public_key = session.find_objects(:CLASS => PKCS11::CKO_PUBLIC_KEY).select { |obj| obj[:LABEL] == key_label}.first
@@ -76,11 +79,9 @@ class Hiera
               puts "Found public key:  #{public_key[:LABEL]}"
              
               if action == :encrypt
-                puts "Session: #{session.info.inspect}"
                 result = session.encrypt(:RSA_PKCS,public_key,text)
               elsif action == :decrypt
-                # Decode Base64 text and decrypt original plaintext and return
-                result = session.decrypt( :RSA_PKCS,private_key,text)
+                result = session.decrypt(:RSA_PKCS,private_key,text)
               end
               session.logout
               result
@@ -91,7 +92,7 @@ class Hiera
               #    pub_key, priv_key = session.generate_key_pair(:RSA_PKCS_KEY_PAIR_GEN,
               #          {:MODULUS_BITS=>2048, :PUBLIC_EXPONENT=>[3].pack("N"), :TOKEN=>false},
               #                {})
-             raise StandardError "Not implemented"
+             raise StandardError, "Not implemented"
           end
          end
       end
